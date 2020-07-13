6082dc000872d21da1e42962c1c1fd2a9ca293d2
MarinaMrd <66686494+MarinaMrd@users.noreply.github.com>
2020-07-13 14:01:07 +0200

    update get-df
    Spaltennamen angepasst an die neue excel datei. Ein paar print-tests durchgeführt und wieder auskommentiert

--- get-df.py
+++ get-df.py
@@ -13,20 +13,22 @@ df = pd.read_excel(url)
 
 print(df.shape)
 
-print(df.info())
-print(df.head())
-print(df.describe())
+#print(df.info())
+#print(df.head())
+#print(df.describe())
 
-df = df[['Entity','Year', 'Population', 'GDP in $','Primary Energy Consumption (terrawatt-hours)']]
+df = df[['Entity','Year', 'Population', 'GDP per capita [$]','PEC [TWh]','Oil','Coal','Gas','Hydropower','Nuclear','Solar','Wind','Other renewables','Biofuels and Waste','Transport S','Industry S','Residential S','Commercial and public services S','Agriculture/Forestry S','Non-energy use S','Non-specified S']]
 
+df = df.rename(index= str, columns={'Entity':'Country', 'Year':'Year', 'Population':'Population','GDP per capita [$]':'GDP','PEC [TWh]':'PEV'})#,'Oil':'Oil','Coal':'Coal','Gas':'Gas','Hydropower':'Hydropower','Nuclear':'Nuclear'})
 
-df = df.rename(index= str, columns={'Entity':'Country', 'Year':'Year', 'Population':'Population','GDP in $':'GDP','Primary Energy Consumption (terrawatt-hours)':'PEV' })
 df = df.set_index(['Year','Country']).sort_index()#Beispiele für slicing: 1. Gesamter DataFrame bis Zeile 100, 2. bestimmtes Jahr 3. bestimmtes Land
 print(df[:100])
-print(df.loc[2010])
-print(df.xs((slice(None), 'Germany')))
+#print(df.loc[2010])
+#print(df.loc[1970])
+#print(df.loc[[1970], ['Transport S', 'Gas', 'PEV']])
+#print(df.xs((slice(None), 'Germany')))
 
 
-df.plot(x='Population', y='PEV', kind='scatter')
-df.plot(x='GDP', y='PEV', kind='scatter')
-plt.show()
+#df.plot(x='Population', y='PEV', kind='scatter')
+#df.plot(x='GDP', y='PEV', kind='scatter')
+#plt.show()
